a073c5f74cb243ddf549d0650bfc39a24c04fac7
Will Saffran <willh.saffran@gmail.com>
2019-02-05 20:18:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  list_of_badge_messages = []
-  attendees.each { |x| list_of_badge_messages << badge_maker(x)}
-  list_of_badge_messages
+  list = []
+  attendees.each { |x| list << badge_maker(x)}
+  list
 end
 
 def assign_rooms(attendees)
-  list_of_badge_messages_and_room_assignments = []
-  attendees.each { |x| list_of_badge_messages_and_room_assignments << "Hello, #{x}! You'll be assigned to room #{attendees.index(x)+1}!"}
-  list_of_badge_messages_and_room_assignments
+  list = []
+  attendees.each { |x| list << "Hello, #{x}! You'll be assigned to room #{attendees.index(x)+1}!"}
+  list
 end
