5b6c1e275a822ee9687ace4768e927c63c2e3c7a
Will Saffran <willh.saffran@gmail.com>
2019-02-05 20:07:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,11 @@ end
 
 def batch_badge_creator(attendees)
   array_of_badge_messages = []
-  
-  
+  i = 0
+  attendees.length.times do
+    array_of_badge_messages.push(badge_maker(attendees[i]))
+    i++
+  end
+  array_of_badge_messages
+end
   
